46e24044044e1670e545e217d29df06759382718
antac69 <angello_04_09@hotmail.com>
2022-01-04 21:44:09 -0500

    avanzando 90%

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
 const fragment = document.createDocumentFragment();
 const template_card = document.getElementById('card').content;
 const dashboard_cards = document.querySelector('.dashboard_cards');
+const filtro = document.querySelector('.dashboard_header_controls');
 let bd;
 const getData = async()=>{
     try{
@@ -21,10 +22,21 @@ const printData = (object = bd,filter = 'weekly')=>{
     Object.values(object).forEach(element => {
         template_card.querySelector('.card').classList =`card card--${element['title'].toLocaleLowerCase().replace(' ', '-')}`
         template_card.querySelector('.card_icon img').src = `./images/icon-${element['title'].toLocaleLowerCase().replace(' ', '-')}.svg`;
-/*         element['timeframes'][filter][0]
-        element['timeframes'][filter][1] */
+        template_card.querySelector('.card_info-title').innerText = element.title;
+        template_card.querySelector('.hours').innerText = `${element['timeframes'][filter]['current']}hrs`
+        template_card.querySelector('.time').innerText = `${element['timeframes'][filter]['previous']}hrs`
+        if (filter === 'weekly'){
+            template_card.querySelector('.type').innerText = 'Week'
+        } 
+        else if(filter === 'daily'){
+            template_card.querySelector('.type').innerText = 'Day'
+        }else{
+            template_card.querySelector('.type').innerText = 'Month'
+        }
         const clone = template_card.cloneNode(true);
         fragment.appendChild(clone);
     });
     dashboard_cards.appendChild(fragment);
 }
+
+filtro.addEventListener('input',(e)=>)
